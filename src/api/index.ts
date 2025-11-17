6eb40231a11ea9794cafe15189eedbb87e5ae6f6
mitteos <dmi3ty@gmail.com>
2025-11-17 17:34:56 +0600

    Update API base URL to production endpoint in index.ts

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:3000/api"; // Замените на ваш реальный URL
+const API_BASE_URL = "https://songless-api.onrender.com/api"; // Замените на ваш реальный URL
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
